8922ea1dc05a71a02a6b2b8ba23c4905baedadbd
anujsaraswat20 <33923329+anujsaraswat20@users.noreply.github.com>
2017-11-29 16:34:04 +0530

    Update HelloWorldTest.java

--- src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
@@ -1,9 +1,19 @@
 package org.jacoco.examples.maven.java;
 
+import junit.framework.Assert;
+import junit.framework.TestCase;
 
+public class HelloWorldTest extends TestCase {
 
-public class HelloWorldTest {
-
+	protected void setUp() throws Exception {
+		
+	}
 	
+	protected void tearDown() throws Exception {
+		
+	}
 	
+	public void test1() {
+		
+	}
 }
